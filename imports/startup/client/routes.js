e0432bd14b3ee11ff72ee25b96c53bc8434c0b20
VANLERBERGHE Michael <michael.vanlerberghe@etu.univ-amu.fr>
2020-01-16 16:29:32 +0100

    timeline ajoutÃ© navbar et timeline bougÃ©s dans body.html

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -10,7 +10,7 @@ import '../../ui/pages/not-found/not-found.js';
 FlowRouter.route('/', {
   name: 'App.home',
   action() {
-    BlazeLayout.render('App_body', { main: 'App_home' });
+    BlazeLayout.render('App_body', { main: 'timeline' });
   },
 });
 
--- imports/ui/pages/home/home.js
+++ imports/ui/pages/home/home.js
@@ -3,3 +3,4 @@ import './home.html';
 import '../../components/hello/hello.js';
 import '../../components/info/info.js';
 import '../../components/navbar/navbar.js';
+import '../../components/timeline/timeline.js';
