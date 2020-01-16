a75b09112011635936c34d68529b76544926806b
VANLERBERGHE Michael <michael.vanlerberghe@etu.univ-amu.fr>
2020-01-16 16:49:36 +0100

    composant post crée

--- imports/ui/components/timeline/timeline.js
+++ imports/ui/components/timeline/timeline.js
@@ -1,1 +1,17 @@
 import './timeline.html';
+
+Template.timeline.helpers({
+	posts: [
+		{ 
+			username: 'Michäel',
+			content: 'Ceci est un nouveau tweet',
+			timestamp: '16/01/2020 16h41'
+		},
+		{
+			username: 'Yann',
+			content: 'Ceci est un autre nouveau tweet',
+			timestamp: '16/01/2020 15h35'
+		}
+		
+	]
+});
