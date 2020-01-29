b9754e6ea6bee832642e61e214db9f31bdd241a7
VANLERBERGHE Michael <michael.vanlerberghe@etu.univ-amu.fr>
2020-01-29 10:48:59 +0100

    update post-form

--- imports/ui/components/post-form/post-form.js
+++ imports/ui/components/post-form/post-form.js
@@ -1,1 +1,11 @@
-import "./post-form.html"
+import "./post-form.html";
+import {Posts} from "../../../../collections/Posts.js";
+
+Template.post_form.events({
+	'submit'(event){
+		event.preventDefault();
+		Posts.insert({
+			content: event.target.content.value
+		});
+	}
+})
