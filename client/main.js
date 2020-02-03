cb2e7d4d627a2c9d10b772bedceafc203767a542
Yblaster <59924957+Yblaster@users.noreply.github.com>
2020-02-03 20:21:26 +0100

    ajout de l'identification par pseudo

--- client/main.js
+++ client/main.js
@@ -2,5 +2,6 @@
 import '../node_modules/jquery/dist/jquery.js';
 import '../node_modules/popper.js/dist/popper.js';
 import '../node_modules/bootstrap/dist/js/bootstrap.js';
+import '/imports/startup/both/accounts-config.js';
 import '/imports/startup/client';
 import '/imports/startup/both';
