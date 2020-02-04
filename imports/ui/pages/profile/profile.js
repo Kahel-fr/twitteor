a4825d35d73e15e20d03c77120f1487048d43756
Kahel <micaoa@gmail.com>
2020-02-04 18:10:12 +0100

    update profile.js

--- imports/ui/pages/profile/profile.js
+++ imports/ui/pages/profile/profile.js
@@ -8,54 +8,8 @@ Template.profile.onCreated(function() {
 Template.profile.helpers({
 	user : () => {
 		//si aucun paramètre n'est passé -> profile de l'utilisateur connecté
-		if(!FlowRouter.getParam("_id"))
-			return Meteor.users.findOne(Meteor.userId());
-		//sinon profile de l'utilisateur avec cet id
-		else{
-			console.log(Meteor.users.findOne({_id: FlowRouter.getParam("_id")}));
+			var id;
+			!FlowRouter.getParam("_id") ? id = Meteor.userId() : FlowRouter.getParam("_id");
 			return Meteor.users.findOne({_id: FlowRouter.getParam("_id")});
-		}
-	},
-	posts:[
-		{ 
-			username: 'Michaël',
-			content: 'Ceci est un nouveau tweet',
-			timestamp: '16/01/2020 16h41'
-		},
-		{ 
-			username: 'Michaël',
-			content: 'Ceci est un nouveau tweet',
-			timestamp: '16/01/2020 16h41'
-		},
-		{ 
-			username: 'Michaël',
-			content: 'Ceci est un nouveau tweet',
-			timestamp: '16/01/2020 16h41'
-		},
-		{ 
-			username: 'Michaël',
-			content: 'Ceci est un nouveau tweet',
-			timestamp: '16/01/2020 16h41'
-		},
-		{ 
-			username: 'Michaël',
-			content: 'Ceci est un nouveau tweet',
-			timestamp: '16/01/2020 16h41'
-		},
-		{ 
-			username: 'Michaël',
-			content: 'Ceci est un nouveau tweet',
-			timestamp: '16/01/2020 16h41'
-		},
-		{ 
-			username: 'Michaël',
-			content: 'Ceci est un nouveau tweet',
-			timestamp: '16/01/2020 16h41'
-		},
-		{ 
-			username: 'Michaël',
-			content: 'Ceci est un nouveau tweet',
-			timestamp: '16/01/2020 16h41'
-		},
-	]
+	}
 });
