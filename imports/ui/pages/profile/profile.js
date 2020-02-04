11437dd50fbd5b4e0962d91966eed6cfd6626e22
Kahel <micaoa@gmail.com>
2020-02-04 19:30:52 +0100

    le profil affiche les tweets de l'user

--- imports/ui/pages/profile/profile.js
+++ imports/ui/pages/profile/profile.js
@@ -1,15 +1,22 @@
+import {Posts} from '../../../../collections/Posts.js'
 import "./profile.html"
 import "../../components/user-card/user-card.js";
+import '../../components/post/post.js';
 
 Template.profile.onCreated(function() {
 	Meteor.subscribe('users.all');
+	Meteor.subscribe('posts.all');
 });
 
 Template.profile.helpers({
-	user : () => {
-		//si aucun paramètre n'est passé -> profile de l'utilisateur connecté
-			var id;
-			!FlowRouter.getParam("_id") ? id = Meteor.userId() : FlowRouter.getParam("_id");
-			return Meteor.users.findOne({_id: FlowRouter.getParam("_id")});
-	}
+	user() {
+		//si aucun paramètre n'est passé -> profile de l'utilisateur connecté sinon -> profile qui correspond à l'id passé en paramètre
+		var id = !FlowRouter.getParam("_id") ?Meteor.userId() : FlowRouter.getParam("_id");
+		console.log(id)
+		return Meteor.users.findOne({_id: id});
+	},
+	posts() {
+		var id = !FlowRouter.getParam("_id") ?Meteor.userId() : FlowRouter.getParam("_id");
+		return Posts.find({author: id}, { sort: { createdAt: -1 } });
+	},
 });
