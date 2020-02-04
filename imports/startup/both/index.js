6edea639d876e43629a54aec1b47eb6cace586d6
Kahel <micaoa@gmail.com>
2020-02-04 18:05:18 +0100

    update profile et publish/subscribe Meteor.users collection

--- imports/startup/both/index.js
+++ imports/startup/both/index.js
@@ -1,2 +1,19 @@
 // Import modules used by both client and server through a single index entry point
 // e.g. useraccounts configuration file.
+
+Meteor.users.allow({
+  insert: function () {
+    return !!Meteor.userId();
+  }
+})
+
+Meteor.users.attachSchema(SchemaPost);
+
+if(Meteor.isServer){
+	Meteor.publish('users.all', function(){
+		return Meteor.users.find({});
+	});
+}
+else{
+	Meteor.subscribe('users.all');
+}
--- imports/ui/pages/profile/profile.js
+++ imports/ui/pages/profile/profile.js
@@ -1,14 +1,20 @@
 import "./profile.html"
 import "../../components/user-card/user-card.js";
 
+Template.profile.onCreated(function() {
+	Meteor.subscribe('users.all');
+});
+
 Template.profile.helpers({
 	user : () => {
 		//si aucun paramètre n'est passé -> profile de l'utilisateur connecté
 		if(!FlowRouter.getParam("_id"))
 			return Meteor.users.findOne(Meteor.userId());
 		//sinon profile de l'utilisateur avec cet id
-		else
-			return Meteor.users.findOne(FlowRouter.getParam("_id"));
+		else{
+			console.log(Meteor.users.findOne({_id: FlowRouter.getParam("_id")}));
+			return Meteor.users.findOne({_id: FlowRouter.getParam("_id")});
+		}
 	},
 	posts:[
 		{ 
